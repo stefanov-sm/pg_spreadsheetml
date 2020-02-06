e293c5639cedb57534755a0d5514faf0453011de
Stefan Stefanov <stefanov.sm@abv.bg>
2020-02-06 17:37:37 +0200

    Update pg_spreadsheetml.sql

--- pg_spreadsheetml.sql
+++ pg_spreadsheetml.sql
@@ -2,47 +2,6 @@
 -- pg_spreadsheetml, S. Stefanov, Feb-2020
 --------------------------------------------------
 
--- helpers
---------------------------------------------------
-
-CREATE OR REPLACE FUNCTION public.xml_escape(s text) RETURNS text LANGUAGE sql IMMUTABLE STRICT AS
-$function$
-  select replace(replace(replace(s, '&', '&amp;'), '>', '&gt;'), '<', '&lt;');
-$function$;
-
---------------------------------------------------
-
-CREATE OR REPLACE FUNCTION public.macro_expand(macro text, args json) RETURNS text LANGUAGE plpgsql IMMUTABLE STRICT AS
-$function$
-declare
-    k text;
-    v text;
-begin
-    for k, v in select "key", "value" from json_each_text(args) loop
-        macro := replace(macro, '__' || upper(k) || '__', coalesce(v, ''));
-    end loop;
-    return macro;
-end;
-$function$;
-
---------------------------------------------------
-
-CREATE OR REPLACE FUNCTION public.json_typeofx(j json) RETURNS text LANGUAGE sql IMMUTABLE AS
-$function$
-select
-  case
-    when json_typeof(j) = 'string' then case 
-      when j::text ~ '^"\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?' then 'datetime'
-      when j::text ~ '^"\d{4}-\d\d-\d\d' then 'date'
-      else 'string' 
-    end
-    else json_typeof(j)
-  end;
-$function$;
-
---------------------------------------------------
--- helpers end
-
 CREATE OR REPLACE FUNCTION public.pg_spreadsheetml(arg_query text, arg_parameters json DEFAULT '{}'::json)
 RETURNS SETOF text LANGUAGE plpgsql SECURITY DEFINER AS
 $function$
