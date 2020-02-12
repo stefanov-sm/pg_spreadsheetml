8f9617bd884029a52bae773e18edadcae66adbda
Stefan Stefanov <stefanov.sm@abv.bg>
2020-02-12 20:55:28 +0200

    Update helpers.sql

--- helpers.sql
+++ helpers.sql
@@ -4,29 +4,29 @@
 
 CREATE OR REPLACE FUNCTION xml_escape(s text)
 RETURNS text LANGUAGE sql IMMUTABLE STRICT AS
-$function$
+$$
   select replace(replace(replace(s, '&', '&amp;'), '>', '&gt;'), '<', '&lt;');
-$function$;
+$$;
 --------------------------------------------------
 
 CREATE OR REPLACE FUNCTION macro_expand(macro text, args json)
 RETURNS text LANGUAGE plpgsql IMMUTABLE STRICT AS
-$function$
+$$
 declare
-    k text;
-    v text;
+  k text;
+  v text;
 begin
-    for k, v in select "key", "value" from json_each_text(args) loop
-        macro := replace(macro, '__' || upper(k) || '__', coalesce(v, ''));
-    end loop;
-    return macro;
+  for k, v in select "key", "value" from json_each_text(args) loop
+    macro := replace(macro, '__' || upper(k) || '__', coalesce(v, ''));
+  end loop;
+  return macro;
 end;
-$function$;
+$$;
 --------------------------------------------------
 
 CREATE OR REPLACE FUNCTION json_typeofx(j json)
 RETURNS text LANGUAGE sql IMMUTABLE AS
-$function$
+$$
 select
   case
     when json_typeof(j) = 'string' then case 
@@ -36,4 +36,4 @@ select
     end
     else json_typeof(j)
   end;
-$function$;
+$$;
