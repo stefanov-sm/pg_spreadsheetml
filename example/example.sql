c166976d749f89b6e941a71cf85f0f8de6b4e21b
Stefan Stefanov <stefanov.sm@abv.bg>
2020-06-01 09:24:48 +0300

    Update example.sql

--- example/example.sql
+++ example/example.sql
@@ -3,7 +3,7 @@ COPY
 (
  SELECT xml_line FROM pg_spreadsheetml
  (
-  $$
+  $query$
   SELECT r.rental_date, r.return_date, c.first_name, c.last_name, c.email, f.title, f.replacement_cost 
     FROM rental r
      INNER JOIN customer c ON r.customer_id = c.customer_id 
@@ -12,8 +12,8 @@ COPY
     WHERE r.return_date - r.rental_date > '__NUMBER_OF_DAYS__'::interval
     AND f.replacement_cost > __COST__
     ORDER by r.return_date - r.rental_date DESC;
-  $$,
+  $query$,
   '{"Number_Of_Days": "7 days", "cost": 15.00}'::json
- ) t(xml_line)
+ ) AS t(xml_line)
 )
-TO '-- path-to --/delme.xml';
+TO '-- path-to --/delme.xml'
