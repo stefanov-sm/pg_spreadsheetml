a8883f4ef6ab414fd3128c6bd866ba01014ae7d1
Luca Ferrari <fluca1978@gmail.com>
2020-07-02 10:54:12 +0200

    Check for unexpanded macros.

--- helpers.sql
+++ helpers.sql
@@ -36,6 +36,13 @@ $$;
   ------------------------------------
    SELECT 40 FROM 30 WHERE 10 like 10
 
+
+
+
+  testdb=> SELECT macro_expand( 'SELECT __FOO__ FROM __bar__ WHERE __i__ like __I__ AND __Z__ = 99',
+                                '{ "I" : "10", "j" : "20", "bar" : "30", "foo" : "40" }'::json );
+  ERROR:  1 macro(s) not expanded, please check your JSON arguments!
+
   */
 create or replace function macro_expand(macro text, args json)
 returns text language plpgsql immutable strict as
@@ -43,6 +50,7 @@ $$
 declare
   k text;
   v text;
+  macro_to_expand int := 0;
 begin
   for k, v in select "key", "value" from json_each_text(args) loop
     macro := regexp_replace( macro
@@ -51,6 +59,13 @@ begin
                               , 'gi' );
     raise debug 'Key [%] = [%] produced [%]', k, v, macro;
   end loop;
+
+  -- check there is no macro without expansion
+  macro_to_expand :=  array_length( regexp_matches( macro, '__[a-z]+__',  'i' ), 1 );
+  if macro_to_expand > 0 then
+    raise exception '% macro(s) not expanded, please check your JSON arguments!', macro_to_expand;
+  end if;
+
   return macro;
 end;
 $$;
